878f93ab12d02de699dc8b9e94191792fea99ae3
neyasbit <kobzevjob@gmail.com>
2022-08-23 16:53:38 +0700

    build src

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,10 @@
+object Modules {
+    object Base {
+        const val CORE = ":core"
+    }
+
+    object Features {
+        private const val MODULE_NAME = ":features"
+        const val TIP_CALC = "$MODULE_NAME:tip-calculator"
+    }
+}
